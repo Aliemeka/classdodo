6c856ef602b48235f0096cd24cae6ffb462cbd5e
Emeka Allison <emekaallison4@gmail.com>
2020-01-10 18:31:26 +0100

    deleted class and replaced it with classroom

---
+++ base/classroom/models.py
@@ -0,0 +1,32 @@
+from django.db import models
+from account.models import Teacher
+
+# Create your models here.
+class Subject(models.Model):
+    subject_name = models.CharField(max_length=20)
+    teacher = models.ForeignKey(Teacher, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.subject_name
+
+    class Meta:
+        verbose_name = ('subject')
+        verbose_name_plural = ('subjects')
+    
+
+
+class Question(models.Model):
+    question = models.TextField()
+    subject = models.ForeignKey(Subject, on_delete=models.CASCADE)
+
+    def __unicode__(self):
+        return self.question
+
+
+class Option(models.Model):
+    option = models.CharField(max_length=120)
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    is_answer = models.BooleanField(default=False)
+
+    def __unicode__(self):
+        return self.option
