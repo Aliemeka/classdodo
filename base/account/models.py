d3a24e76d846bab4ebe8e9aba0e77951db5c3baf
Emeka Allison <emekaallison4@gmail.com>
2020-01-11 18:43:45 +0100

    small tweaks to .gitignore

--- base/account/models.py
+++ base/account/models.py
@@ -1,3 +1,27 @@
 from django.db import models
-
+from django.contrib.auth.models import AbstractUser
 # Create your models here.
+
+
+class AppUser(AbstractUser):
+    is_teacher = models.BooleanField()
+    is_student = models.BooleanField()
+
+    def __str__(self):
+        return '%s %s' %(self.first_name, self.last_name)
+
+class Teacher(models.Model):
+    user = models.OneToOneField(AppUser, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return '%s %s' %(self.user.first_name, self.user.last_name)
+    
+
+class Student(models.Model):
+    user = models.OneToOneField(AppUser, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return '%s %s' %(self.user.first_name, self.user.last_name)
+    
+    def __unicode__(self):
+        return '%s %s' %(self.user.first_name, self.user.last_name)
