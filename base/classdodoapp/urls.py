107cf3f19ca59ed918ff6fde81a842e8a3cb7b3c
Emeka Allison <emekaallison4@gmail.com>
2020-01-14 10:05:04 +0100

    Classdodo url update

--- base/classdodoapp/urls.py
+++ base/classdodoapp/urls.py
@@ -4,5 +4,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('classroom.urls')),
+    path('accounts/', include('account.urls')),
     path('records/', include('score.urls'))
 ]
