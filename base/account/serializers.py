ecd6ba9b36bc5635c0912587b1c1b46fd3d7c43c
Emeka Allison <emekaallison4@gmail.com>
2020-01-16 22:10:56 +0100

    Edited account serializers

--- base/account/serializers.py
+++ base/account/serializers.py
@@ -2,12 +2,19 @@ from rest_framework.serializers import ModelSerializer
 from rest_framework import serializers
 from rest_auth.registration.serializers import RegisterSerializer
 from .models import AppUser
+#from score.serializers import RecordSerializer
 
 class UserSerializer(ModelSerializer):
     class Meta:
         model = AppUser
         fields = ('first_name', 'last_name', 'email', 'username', 'password', 'is_student', 'is_teacher')
 
+    # def get_record(obj, self):
+    #     users = AppUser.objects.all()
+    #     for user in users:
+    #         if user.is_student:
+    #             record = RecordSerializer(obj.record.all(), many=False).data
+    #             return record
 
 class CustomRegisterSerializer(RegisterSerializer):
     is_teacher = serializers.BooleanField()
