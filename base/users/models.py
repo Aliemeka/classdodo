ccad17e8624004cc3f325f124f266fe6da69af96
Emeka Allison <emekaallison4@gmail.com>
2020-10-28 10:32:13 +0100

    Update codebase

--- base/users/models.py
+++ base/users/models.py
@@ -8,20 +8,20 @@ class AppUser(AbstractUser):
     is_student = models.BooleanField()
 
     def __str__(self):
-        return '%s %s' %(self.first_name, self.last_name)
+        return f'{self.user.first_name} {self.user.last_name}'
 
 class Teacher(models.Model):
     user = models.OneToOneField(AppUser, on_delete=models.CASCADE)
 
     def __str__(self):
-        return '%s %s' %(self.user.first_name, self.user.last_name)
+        return f'{self.user.first_name} {self.user.last_name}'
     
 
 class Student(models.Model):
     user = models.OneToOneField(AppUser, on_delete=models.CASCADE)
 
     def __str__(self):
-        return '%s %s' %(self.user.first_name, self.user.last_name)
+        return f'{self.user.first_name} {self.user.last_name}'
     
     def __unicode__(self):
-        return '%s %s' %(self.user.first_name, self.user.last_name)
+        return f'{self.user.first_name} {self.user.last_name}'
