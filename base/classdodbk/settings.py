b82ffb1513f180db6823d85e6e9ef80e809b9ffd
Emeka Allison <emekaallison4@gmail.com>
2020-11-03 23:48:27 +0100

    Added env reading

--- base/classdodbk/settings.py
+++ base/classdodbk/settings.py
@@ -4,6 +4,7 @@ import django_heroku
 import environ
 
 env = environ.Env()
+environ.Env.read_env()
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
